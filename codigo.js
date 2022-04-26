fc7a2b5b734ccab95ccba76c37964fee3615f906
CAMILO BARBERO <CAMILOBARBERO3@GMAIL.COM>
2022-04-26 16:48:58 -0300

    cambios

---
+++ codigo.js
@@ -0,0 +1,27 @@
+// Resolver problemas logicos o matematicos
+// la programacion es una herramienta para reoslver problemas
+// pensamiento computacional:
+// 1) descomposicon: descomponer el problema
+// 2) patron: similitudes
+// 3) abstraccion: achicar el problema, eliminar lo superfluo
+// 4) algoritmos: pasos para resolver el problema
+
+console.log("Hola mundo")
+
+/*
+variables? espacio en memeoria donde se alamacena un valor
+que es? un cajon 
+
+como armo una variable?
+let nombredelavariable = "valor"
+let nombre = "camilo"
+
+= es el operador de asignacion
+"var" << para crear variables, pero ahora se usa mas "let"
+
+las variables tienen diferentes tipos de datos:
+numericos: enteros (integer) o decimales (float)
+strings: cadena de caracteres
++ > suma o concatenacion de strings
+
+*/
