5237af4b940786e97176c10645cdd61ebb015846
CAMILO BARBERO <CAMILOBARBERO3@GMAIL.COM>
2022-05-10 10:52:56 -0300

    hola

---
+++ funciones.js
@@ -0,0 +1,24 @@
+// funcion: conjunto de instrucciones que se agrupan para realizar una tarea
+
+// las puedad declarar (crearlas) o invocarlas (utilizarlas)
+
+// declarar permite juntar todas estas acciones a una linea de codigo o poca linea de codigo,
+// de esta manera no se tendran que escribir todas las instrucciones cada vez
+// que se quiera realizar la accion.
+
+// principio KISS --> llevar las tareas a lo minimo que se pueda hacer, 'keep it simple'
+// principio YAGNI --> vas creando la funcion a medida que la necesitas. 
+// cuando no la necesito, no la creo. asi no tengo codigo de mas
+
+function saludar(){ // estas declarando la funcion
+    console.log("Hola, estudiante")
+}
+
+// invoco la funcion:
+saludar()
+
+// la estructura de una funcion va asi:
+
+// function nombre (){ instrucciones }
+// el nombre de la funcion es unico para cada funcion
+
