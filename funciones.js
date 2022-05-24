09f5236d4e1aa81a79276b2a5c338fdedc459ede
CAMILO BARBERO <CAMILOBARBERO3@GMAIL.COM>
2022-05-24 11:06:37 -0300

    cambios

--- funciones.js
+++ funciones.js
@@ -158,3 +158,87 @@ return costoTotal
 }
 
 mostrar(calcularCostos("limpieza de cutis", "uñas esculpidas", "uñas encarnadas"))
+
+
+// variable: un espacio en memoria
+
+let nombre = "Esteban";
+let edad = 29;
+let cargo = "profesor"
+
+let profesor = ["Esteban",29,"profesor", true] // esto es un array (o lista como vimos en python)
+//                  0      1    2         3       cantidad de elementos: n-1  , los array empeizan en 0
+console.log(profesor)
+console.log(profesor[0]) // va a mostrar 'Esteban"
+
+for(let i = 0; i < 4; i++){         // i++ es lo mismo que decir i = i + 1 y asi se va acumulando
+    console.log(profesor[i])
+}
+
+// yo puedo acceder a las propeidades de los arrays
+
+// propiedades:
+// largo --> 
+for(let i = 0; i < profesor.length; i++){             // profesor.length significa el largo del array
+    console.log(profesor[i])
+}
+
+for(let i = 0; i < 4; i++){         
+    profesor[i] = profesor[i] + 1
+}
+
+// Metodos que me permiten cambiar mi array
+// Push:
+
+
+profesor.push("El mejor") // agrega un elemento al final del array
+console.log(profesor)
+
+profesor.unshift("Hola") // agrega un elemento al principio del array
+console.log(profesor)
+
+let ultimo = profesor.pop() // guardas el ultimo elemento del array adentro de esta variable llamada "ultimo"
+console.log(ultimo)
+
+let primero = profesor.shift() // guardas el primer elemento del array adentro de esta variable llamada "primero"
+console.log(ultimo)
+
+console.table(profesor) // transforma un array en tabla
+
+// splice (splice mdm) documentacion de javascript
+
+profesor.splice(indice, )
+
+profesor.splice(1, 1) // 
+console.log(profesor)
+
+profesor.splice(profesor.length/2 , 1) // saca el elemento del array que este en el medio
+
+let mitad = profesor.splice(profesor.length/2, profesor.length)
+console.log(mitad)
+
+let equipo = ["esteban", "dani", "nicky", "guille"]
+console.log(equipo)
+let frase = equipo.join('mulea a') // agrega al array "mule a"
+console.log(frase)
+
+let perros = ["firulais", "odin"]
+let gatos = ["dona, 'michi"]
+
+let mascotas = perros.concat(gatos) // junta los dos arrays uno despues del otro respectivamente
+console.log(mascotas)
+
+let letras = ["a", "b", "c", "d", "e"]
+let nuevo = letras.slice(letras.length/2, letras.length) // los corta y cuando el array es impar redondea hacia abajo
+console.log(letras)
+console.log(nuevo)
+console.long(letras.reversed()) // invierte el orden del array
+
+if (letras.includes("a")){ // te dice si algo o no esta en el array
+    console.log("la letra esta en un array")
+}
+
+console.log(letras.indexOf('a')) // te devuelve en que indice esta el elemento del array
+console.log(letras.indexOf('f')) // si el elemento no esta en el array te va a devolver el valor -1
+
+
